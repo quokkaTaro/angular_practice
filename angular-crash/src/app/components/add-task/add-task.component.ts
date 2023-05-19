d93337296d658b39fa95fcc6455e0f2d6f9119d5
quokkaTaro <133376194+quokkaTaro@users.noreply.github.com>
2023-05-19 13:35:15 +0800

    ✨feature: submit & add btn
    submit task
    add&close button

--- angular-crash/src/app/components/add-task/add-task.component.ts
+++ angular-crash/src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { outputAst } from '@angular/compiler';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-add-task',
@@ -6,13 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-task.component.css'],
 })
 export class AddTaskComponent implements OnInit {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+
   text: string;
   day: string;
   reminder: boolean = false;
+  showAddTask: boolean;
+  subscription: Subscription;
 
-  constructor() {}
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
 
   ngOnInit(): void {}
 
-  onSubmit() {}
+  onSubmit() {
+    if (!this.text) {
+      alert('Plz add a task!');
+      return;
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder,
+    };
+
+    this.onAddTask.emit(newTask);
+
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
 }
--- angular-crash/src/app/components/header/header.component.ts
+++ angular-crash/src/app/components/header/header.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UiService } from 'src/app/services/ui.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit{
+export class HeaderComponent implements OnInit {
   title: string = 'Task Tracker';
+  showAddTask: boolean;
+  subscription: Subscription;
 
-  constructor(){}
-
-  ngOnInit():void{}
-
-  toggleAddTask(){
-    console.log('toggle');
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
   }
 
+  ngOnInit(): void {}
+
+  toggleAddTask() {
+    this.uiService.toggleAddTask();
+  }
 }
